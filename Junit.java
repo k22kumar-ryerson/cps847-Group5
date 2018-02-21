ccb312a34e1c667ba0e5f0863aadb13b56b1525e
k22kumar-ryerson <36056519+k22kumar-ryerson@users.noreply.github.com>
2018-02-21 12:59:05 -0500

    Add files via upload

---
+++ Junit.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Write a description of class Junit here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Junit
+{
+    
+    public void testAdd() {
+    
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine",str);
+    
+    }
+    
+    
+    
+    
+}
